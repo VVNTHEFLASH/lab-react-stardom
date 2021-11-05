ae01a77fbc8a8094f36f066b36b7614001e8f271
VVNTHEFLASH <vishnurockhunters@gmail.com>
2021-11-05 23:26:04 +0530

    Done

---
+++ stardom/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Displayprostar from './component/displayprostar';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='head'>
+        <h1>ProStars</h1>
+      </div>
+      <Displayprostar/>
+    </div>
+  );
+}
+
+export default App;
